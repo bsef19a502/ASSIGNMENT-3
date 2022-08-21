82983dd26c0129dfd5c48b6fe749455a2c9547cf
MAHNOOR SHOUKAT <bsef19a502@pucit.edu.pk>
2022-08-21 23:30:51 +0500

    Student class updated

--- app/src/main/java/org/haqnawaz/assignment_database/Student.java
+++ app/src/main/java/org/haqnawaz/assignment_database/Student.java
@@ -1,4 +1,49 @@
 package org.haqnawaz.assignment_database;
 
 public class Student {
+    private int ID;
+    private String Name;
+    private String RollNumber;
+    private boolean isEnroll;
+
+    @Override
+    public String toString() {
+        return "StudentModel{" +
+                "name='" + Name + '\'' +
+                ", rollNumber=" + RollNumber +
+                ", isEnroll=" + isEnroll +
+                '}';
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getRollNumber() {
+        return RollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        RollNumber = rollNumber;
+    }
+
+    public boolean isEnroll() {
+        return isEnroll;
+    }
+
+    public void setEnroll(boolean enroll) {
+        isEnroll = enroll;
+    }
 }
